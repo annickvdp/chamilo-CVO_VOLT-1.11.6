7b4f0250ae30806f0008480c3e991f24cf15b78b
annickvdp <annick.van.den.put@cvovolt.be>
2018-05-18 13:23:52 +0200

    commit10

---
+++ main/lang/dutch2/trad4all.inc.php
@@ -0,0 +1,30 @@
+<?php
+$AddAssignmentPage="Voeg de geüploade studentenpublicaties toe aan dit hoofdstuk.";
+$HWork="Hulp bij studentenpublicaties";
+$Student_publication="Opdrachten\n                        ";
+$StudentPublications="Opdrachten\n                        ";
+$ToolStudentPublication="Opdrachten\n                        ";
+$Work="Opdrachten";
+$Works="Opdrachten";
+$NewStudentPublicationCreated="De nieuwe opdracht is aangemaakt\n                        ";
+$WorkEmailAlertActivate="opdracht";
+$WorkEmailAlertDeactivate="opdracht";
+$TitleManipulateStudentPublication="Opdracht wijzigen             ";
+$StudentPublicationSelectionForGradebookTitle="Opdrachten voor in het puntenboek            ";
+$PublicationStartDate="                        ";
+$PublicationEndDate="                        ";
+$MoveCurrentStudentPublication="Wijzig de opdracht van de huidige student                        ";
+$EditCurrentStudentPublication="Wijzig de opdracht van de huidige student                        ";
+$LearningPath="Leerpaden";
+$Learnpath="Leerpaden";
+$Scormtool="Leerpaden";
+$ToolLearnpath="Leerpaden";
+$ToolGradebook="Puntenboek";
+$Gradebook="Puntenboek";
+$GradebookActivation="Puntenboek activeren";
+$MyGradebook="Mijn Puntenboek";
+$TabsMyGradebook="Mijn Puntenboek";
+$ThematicDetailsDescription="Details van onderwerpen en hun respectievelijke plan en voortgang. De docent kan een onderwerp als afgesloten markeren (hierbij worden alle voorgaande onderwerp als afgesloten getoond).";
+$ClickOnTheLearnerViewToSeeYourLearningPath="Klik op de knop [Bekijk als student] rechtsboven om het leerpad te zien.";
+$SwitchToStudentView="Bekijk als student";
+$SwitchToTeacherView="Bekijk als cursusbeheerder";
